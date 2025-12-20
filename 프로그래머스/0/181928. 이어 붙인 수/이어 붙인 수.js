8b0f6abe70fa2284e20bcee4f05300197b7ce6a9
Sienna <sgoldenbird@gmail.com>
2025-12-20 16:03:10 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let odd ='';
+    let even = '';
+    
+    for (const num of num_list){
+        if(num%2 === 0){
+            even += num;
+        } else {
+            odd += num;
+        }   
+    }
+    
+    return Number(odd) + Number(even);
+}
