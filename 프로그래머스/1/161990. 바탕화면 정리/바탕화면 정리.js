2a41981b88490f00390aa742ff1232c725b145bb
Sienna <sgoldenbird@gmail.com>
2026-09-13 19:25:07 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.28 ms, Memory: 43.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,19 @@
+function solution(wallpaper) {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+    
+    for (let i = 0; i < wallpaper.length; i++) {
+        for (let j = 0; j < wallpaper[i].length; j++) {
+            if (wallpaper[i][j] === '#') {
+                minX = Math.min(minX, i);
+                minY = Math.min(minY, j);
+                maxX = Math.max(maxX, i);
+                maxY = Math.max(maxY, j);
+            }
+        }
+    }
+    
+    return [minX, minY, maxX + 1, maxY + 1];
+}
